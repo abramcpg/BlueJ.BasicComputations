9c594281a48aa60757074e820967cd2434729c0c
Abram <50758769+abramcpg@users.noreply.github.com>
2019-05-23 19:15:30 -0400

    Update LargestInteger.java

--- LargestInteger.java
+++ LargestInteger.java
@@ -1,13 +1,25 @@
- 
+ import java.util.Scanner;
 
-
-public class LargestInteger {
-
-    public Integer findLargestNumberUsingConditional(Integer[] integers){
-        return null;
+public class LargestInteger 
+{
+    
+    
+    public Integer findLargestNumberUsingConditional(Integer[] integers)
+    {
+        
+        
+        
+        
+        return (integers[0] > integers[1]) ? ((integers[0] > integers[2] ) 
+        ?integers[0]:integers[2]):((integers[1] > integers[2])? 
+        integers[1] : integers[2]);
     }
 
-    public Integer findLargestNumberUsingMathMax(Integer[] integers){
-        return null;
+    public Integer findLargestNumberUsingMathMax(Integer[] integers)
+    {
+        
+        
+        
+        return Math.max(integers[0],Math.max(integers[1],integers[2]));
     }
 }
