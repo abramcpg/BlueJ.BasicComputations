93ee8b6697dda186395d58135e1260ffa2b9fa56
Abram <50758769+abramcpg@users.noreply.github.com>
2019-05-23 16:31:58 -0400

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,12 +1,18 @@
- 
-
-
 import java.math.BigInteger;
 
-public class Factorial {
+public class Factorial 
+{
 
-    public BigInteger factorialOf(Integer value){
-        return null;
+    public BigInteger factorialOf(Integer value)
+    {
+        BigInteger factorial = BigInteger.ONE;
+        
+        for (int i = value; i > 0; i--) 
+        {
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+        
+        return factorial;
     }
 
 }
